ac2b24c8e747983bc8b8364326ed82184a967420
Akhyar <ayankcell@gmail.com>
2023-07-11 16:35:18 +0700

    dynamic seo

--- pages/[page].vue
+++ pages/[page].vue
@@ -30,4 +30,12 @@ const route = useRoute()
 const slug = route.params.page;
 
 const {data} = await useAsyncData('page', () => queryContent(slug).findOne())
+
+useSeoMeta({
+  title: `${data.value.title} - Kurajin`,
+  ogTitle: `${data.value.title} - Kurajin`,
+  description: 'Kurajin Kreasi Mandiri jasa pembuatan website gratis untuk UKM, Kelas Online Digital Marketing & Web Programming dan Jasa Kelola Website',
+  ogDescription: 'Kurajin Kreasi Mandiri jasa pembuatan website gratis untuk UKM, Kelas Online Digital Marketing & Web Programming dan Jasa Kelola Website',
+  
+})
 </script>
